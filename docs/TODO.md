e8a75a35f93839edffa42eb77c97b167e9c31075
Autoraem <autoraem@gmail.com>
2025-10-22 01:17:22 -0400

    ALU

---
+++ docs/TODO.md
@@ -0,0 +1,17 @@
+## TODO
+# General Flow:
+1. ALU
+2. RF
+3. Control Logic
+4. Immediate Generator
+5. PC
+6. Instruction Memory 
+7. Data Memory (How to simulate ^?)
+8. Datapath
+    8.1.1 Branches and Jumps
+9. Top Level
+10. Pipelining
+11. Beyond
+# Other Things
+Atomic Instructions?
+Fence, Ecall, EBREAK, Traps, Interrupts?
--- rtl/alu.sv
+++ rtl/alu.sv
@@ -2,24 +2,36 @@ module alu(
     input   logic [31:0]    A,
     input   logic [31:0]    B,
     input   logic [3:0]     Opcode,
-    output  logic [31:0]    Q
-    // Considerations for zero, negative, overflow flags? Useful for future additions.
+    output  logic [31:0]    Q,
+    output  logic          Zero,   
+    output  logic          Neg,    
+    output  logic          Overflow
 );  
+//ADD, SUB, AND, OR, XOR, SHIFT (R, L, Arthimetic), SLT (signed, unsigned)
+//Immediate versions, values stored in B, need immediate generator
+//Values in A and B are data not addresses
+logic [4:0] shamt; //shift amount
 
-    always_comb begin
-        case (Opcode)
-            4'b0000: Q = A + B;          // ADD
-            4'b0001: Q = A - B;          // SUB
-            4'b0010: Q = A & B;          // AND
-            4'b0011: Q = A | B;          // OR
-            4'b0100: Q = A ^ B;          // XOR
-            4'b0101: Q = ~(A | B);       // NOR
-            4'b0110: Q = A << B[4:0];    // SLL
-            4'b0111: Q = A >> B[4:0];    // SRL
-            4'b1000: Q = $signed(A) >>> B[4:0]; // SRA
-            4'b1001: Q = (A < B) ? 32'b1 : 32'b0; // SLT
-            default: Q = 32'b0;           // NOP or undefined operation
-        endcase
+always_comb begin
+    shamt = B[4:0];
+    case (Opcode)
+        4'd0: Q = A + B; //ADD
+        4'd1: Q = A - B; //SUB
+        4'd2: Q = A & B; //AND
+        4'd3: Q = A | B; //OR
+        4'd4: Q = A ^ B; //XOR
+        4'd5: Q = A << shamt; //SLL 
+        4'd6: Q = A >> shamt; //SRL 
+        4'd7: Q = A >>> shamt; //SRA 
+        4'd8: Q = ($signed(A) < $signed(B)) ? 1 : 0; //SLT
+        4'd9: Q = ($unsigned(A) < $unsigned(B)) ? 1 : 0; //SLTU
+        default: Q = 32'b0;
+    endcase
+    //Flags
+    Zero = (Q == 32'b0) ? 1'b1 : 1'b0;
+    Neg = Q[31];
+    Overflow = 1'b0; // Placeholder
+end
 
 
 endmodule alu();
